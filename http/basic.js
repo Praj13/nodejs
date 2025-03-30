b51f7a613b9492d582ab15c529cd89927337c627
Praj13 <rajbansiprajwol@gmail.com>
2025-03-30 08:35:51 +0545

    basic http request and respone

---
+++ http/basic.js
@@ -0,0 +1,23 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    if(req.rul==='/'){
+    res.writeHead(200,{"content-type":"text/plain"});
+    res.end("hello world and welcome to home page");
+    }
+    if(req.rul==='/about'){
+        res.writeHead(200,{"content-type":"text/plain"});
+        res.end("welcome to about page");
+        }
+        else if(req.rul==='/contact'){
+            res.writeHead(200,{"content-type":"text/plain"});
+            res.end("welcome to contact page");
+            }
+            else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Page Not Found</h1>');
+            }
+})
+
+server.listen(3000,()=>{
+    console.log("server is running at port 3000");
+})
