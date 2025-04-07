230af90584f2b29d84a2da3ed40951317fbacd7f
Praj13 <rajbansiprajwol@gmail.com>
2025-04-07 15:28:47 +0545

    rest api development initials

---
+++ Rest-API-Development/app.js
@@ -0,0 +1,19 @@
+const express= require("express")
+const app=express();
+
+app.use(express.json());
+
+let books=[
+    {
+        id:"1",
+        title:"book-1"
+    },
+    {
+        id:"2",
+        title:"book-2"
+    },
+    {
+        id:"3",
+        title:"book-3"
+    },
+]
