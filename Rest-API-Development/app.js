b291c9ea3faf829d0a02803bd6148b837efbad46
Praj13 <rajbansiprajwol@gmail.com>
2025-04-07 16:23:32 +0545

    get and post

--- Rest-API-Development/app.js
+++ Rest-API-Development/app.js
@@ -17,3 +17,39 @@ let books=[
         title:"book-3"
     },
 ]
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:"welcome to our bookstore"
+    })
+})
+
+app.get('/get',(req,res)=>{
+    res.json(books);
+})
+
+app.get('/get/:id',(req,res)=>{
+    const book=books.find(items=>items.id===req.params.id)
+    if(book){
+    res.status(200).json(book);
+
+    }
+    res.status(404).json("book not found");
+})
+
+//add a new book
+app.post('/add',(req,res)=>{
+    const newBook=
+        {
+            id:books.length+1,
+            title:`book-${books.length+1}`
+        }
+    books.push(newBook);
+    res.status(200).json({
+        data:newBook,
+        message:"new book added"
+    })
+})
+app.listen(3000,()=>{
+    console.log("server is running at port 3000");
+})
