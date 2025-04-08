23118d11fc905d497fe82fdb4babd13d3f9a5240
Praj13 <rajbansiprajwol@gmail.com>
2025-04-08 12:41:27 +0545

    2 data inserted

--- mongodb-basics/app.js
+++ mongodb-basics/app.js
@@ -1,1 +1,49 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://rajbansiprajwol:Bx6WKjpPvEzDaC13@cluster0.yvs7cjn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+).then(()=>{console.log("database connected")}).catch((e)=>(console.log(e)))
+
+const userSchema=new mongoose.Schema({
+    name: String,
+    email: String,
+    age:Number,
+    isActive:Boolean,
+    tags: [String],
+    createdAt: {type:Date,default:Date.now}
+})
+
+const User=mongoose.model('User',userSchema);
+
+async function runQueryExamples() {
+    try{
+        // const newUser=await User.create(
+        //     {
+        //         name: "Prajwol Rajbansi",
+        //         email: "rajbansiprajwlll@gmail.com",
+        //         age:21,
+        //         isActive:true,
+        //         tags: ['developer','designer','manager'],
+        //     }
+        // )
+
+        const newUser=new User(
+            {
+                name: "binesh shrestha",
+                email: "haita@gmail.com",
+                age:21,
+                isActive:true,
+                tags: ['developer','designer','ok'],
+            }
+        )
+        await newUser.save();
+        console.log("new user created",newUser)
+    }
+    catch(e){
+        console.log('error=>',e)
+    }
+    finally{
+        await mongoose.connection.close();
+    }
+}
+
+runQueryExamples();
