06bed84ed89c3fa0d1dfe12fb92a8184085d79a2
Praj13 <rajbansiprajwol@gmail.com>
2025-04-10 10:21:01 +0545

    learnt mongodb basics

--- mongodb-basics/app.js
+++ mongodb-basics/app.js
@@ -27,27 +27,27 @@ async function runQueryExamples() {
 
         // const newUser=new User(
         //     {
-        //         name: "binesh shrestha",
-        //         email: "haita@gmail.com",
-        //         age:21,
+        //         name: "travis scoot",
+        //         email: "yestaiho@gmail.com",
+        //         age:31,
         //         isActive:true,
         //         tags: ['developer','designer','ok'],
         //     }
         // )
 
-        // const newUser=new User(
-        //     {
-        //         name: "votey don",
-        //         email: "votey1@gmail.com",
-        //         age:20,
-        //         isActive:true,
-        //         tags: ['designer'],
-        //     }
-        // )
+        const newUser=new User(
+            {
+                name: "updated user",
+                email: "updated@gmail.com",
+                age:100,
+                isActive:true,
+                tags: ['designer'],
+            }
+        )
 
 
-        // await newUser.save();
-        // console.log("new user created",newUser)
+        await newUser.save();
+        console.log("new user created",newUser) 
 
         // const allUsers= User.find({});
         // console.log(allUsers);
@@ -55,11 +55,52 @@ async function runQueryExamples() {
         // const getUserOfActiveFalse=await User.find({isActive:false})
         // console.log(getUserOfActiveFalse);
 
+        //return last created user 
+        // const getLastCreatedUserByUserID=await User.findById(newUser._id);
+        // console.log(getLastCreatedUserByUserID);
+
 
         //findOne returns the first found data which matches the given criteria
-        const getVoteyDon=await User.findOne({name:'votey don'})
-        console.log(getVoteyDon);
+        // const getVoteyDon=await User.findOne({name:'votey don'})
+        // console.log(getVoteyDon);
+
+        // const Praj=await User.findById("67f4c6c7af3dc9311f449f36");
+        // console.log(Praj)
+
+        // const selectedFields=await User.find().select("name email -_id");
+        // console.log(selectedFields);
+
+        // const limitedUsers=await User.find().limit(5).skip(1);
+        // console.log(limitedUsers);
+
+        //this will sort the list of the users in descending order of their age.
+        // const sortedUsers=await User.find().sort({age:-1});
+        // console.log(sortedUsers);
+
+          //this will sort the list of the users in ascending order of their age.
+        //   const sortedUsers2=await User.find().sort({age:1});
+        //   console.log(sortedUsers2);
+
+        //count the documents who are active
+        // const countDocuments=await User.countDocuments({isActive:true});
+        // console.log(countDocuments);
+
+        //count the documents who are inactive
+        // const countDocuments2=await User.countDocuments({isActive:false});
+        // console.log(countDocuments2);
+
+        //deleting an user
+        // const deleteduser=await User.findByIdAndDelete(newUser._id);
+        // console.log(`user deleted->${deleteduser}`);
+
+        //updating
+        const updatedUser=await User.findByIdAndUpdate(newUser._id,{
+            $set:{age:110},
+            $push:{tags:'updated'}
+        }, {new:true});
+        console.log("user updated",updatedUser);
     }
+    
     catch(e){
         console.log('error=>',e)
     }
