46ebd5c66eb0ee94873dd871938dd37745f713d4
Praj13 <rajbansiprajwol@gmail.com>
2025-04-15 16:54:44 +0545

    routes

--- bookstore-api/server.js
+++ bookstore-api/server.js
@@ -7,9 +7,12 @@ const bookRoutes=require('./routes/book-routes')
 const app=express();
 const PORT=process.env.PORT;
 connectToDB();
+
+//middleware
 app.use(express.json());
 
-
+//routes
+app.use('/api/books',bookRoutes)
 
 app.listen(PORT,()=>{
     console.log("server's running at port",PORT);
