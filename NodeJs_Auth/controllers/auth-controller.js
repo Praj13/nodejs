676e9b7660ca4292ee07afe87a88e1f024ca62ad
Praj13 <rajbansiprajwol@gmail.com>
2025-04-28 15:39:10 +0545

    controllers

---
+++ NodeJs_Auth/controllers/auth-controller.js
@@ -0,0 +1,28 @@
+
+//register controller
+const registerUser= async (req ,res)=>{
+    try{
+
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).json({
+            success:false,
+            message:'some error occured, please try again later'
+        })
+    }
+}
+
+//login controller
+const loginUser= async (req ,res)=>{
+    try{
+
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).json({
+            success:false,
+            message:'some error occured, please try again later'
+        })
+    }
+}
