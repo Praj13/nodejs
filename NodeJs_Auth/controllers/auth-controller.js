fd938ead0eb3794b9a3e75e0c4aa078fb7e2d10a
Praj13 <rajbansiprajwol@gmail.com>
2025-05-03 08:22:21 +0545

    home and admin routes

--- NodeJs_Auth/controllers/auth-controller.js
+++ NodeJs_Auth/controllers/auth-controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 //register controller
 const registerUser = async (req, res) => {
@@ -58,17 +58,102 @@ const registerUser = async (req, res) => {
 
 
 //login controller
-const loginUser= async (req ,res)=>{
-    try{
 
-    }
-    catch(e){
-        console.log(e);
-        res.status(500).json({
-            success:false,
-            message:'some error occured, please try again later'
-        })
-    }
-}
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-module.exports={registerUser, loginUser}
+    //find if the current user is exists in database or not
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: `User doesn't exists`,
+      });
+    }
+    //if the password is correct or not
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials!",
+      });
+    }
+
+    //create user token
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+        username: user.username,
+        role: user.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "30m",
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successful",
+      accessToken,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured! Please try again",
+    });
+  }
+};
+
+const changePassword = async (req, res) => {
+  try {
+    const userId = req.userInfo.userId;
+
+    //extract old and new password;
+    const { oldPassword, newPassword } = req.body;
+
+    //find the current logged in user
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    //check if the old password is correct
+    const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Old password is not correct! Please try again.",
+      });
+    }
+
+    //hash the new password here
+    const salt = await bcrypt.genSalt(10);
+    const newHashedPassword = await bcrypt.hash(newPassword, salt);
+
+    //update user password
+    user.password = newHashedPassword;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password changed successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured! Please try again",
+    });
+  }
+};
+module.exports = { registerUser, loginUser };
